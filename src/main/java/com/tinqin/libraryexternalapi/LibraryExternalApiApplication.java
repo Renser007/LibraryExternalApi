c360918b5f54b56c813a2a5aefaf8bdb65f499ee
Vladimir Andreev <92325644+Renser007@users.noreply.github.com>
2022-08-03 12:38:25 +0300

    no message

---
+++ src/main/java/com/tinqin/libraryexternalapi/LibraryExternalApiApplication.java
@@ -0,0 +1,13 @@
+package com.tinqin.libraryexternalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryExternalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryExternalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tinqin/libraryexternalapi/LibraryExternalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinqin.libraryexternalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryExternalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
